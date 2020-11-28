fe41aa79f284b6011df89d02247b56ec90db7b86
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2020-11-28 11:07:41 +0800

    Update 10.34.cpp

--- ch10/10.34.cpp
+++ ch10/10.34.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 #include <iostream>
-
+#include <iterator>
+using namespace std;
 int main() {
-  //ostream_iterator <int> out(cout," ");
+  ostream_iterator <int> out(cout," ");
   std::vector<int> vi{0, 1, 2, 3, 4, 5};
-  for (auto it = vi.crbegin(); it != vi.crend(); ++it)
-    std::cout << *it << " ";
-  std::cout << std::endl;
-  // copy(vi.crbegin(),vi.crend(),out);
+//   for (auto it = vi.crbegin(); it != vi.crend(); ++it)
+//     std::cout << *it << " ";
+//   std::cout << std::endl;
+  copy(vi.crbegin(),vi.crend(),out);
   return 0;
 }
