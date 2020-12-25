d2cdb0e6058ad1f7198eb3a2830020179e02e932
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2020-12-25 15:33:57 +0800

    Update StrVec.h

--- ch13/13.40/StrVec.h
+++ ch13/13.40/StrVec.h
@@ -7,16 +7,16 @@
 
 class StrVec {
 public:
-  typedef size_t size_type;
-  StrVec() : first_elem(nullptr), first_free(nullptr), cap(nullptr) { }
-  StrVec(std::initializer_list<std::string>);
+  typedef size_t size_type;  //1.typedef 
+  StrVec() : first_elem(nullptr), first_free(nullptr), cap(nullptr) { } //2.默认构造函数
+  StrVec(std::initializer_list<std::string>); //3.重构构造函数
 
-  StrVec(const StrVec &);
-  ~StrVec();
-  StrVec &operator=(const StrVec &);
+  StrVec(const StrVec &);//拷贝构造函数
+  ~StrVec();//析构函数
+  StrVec &operator=(const StrVec &);//拷贝赋值运算符
 
-  void push_back(const std::string &s);
-  void pop_back();
+  void push_back(const std::string &s);//把一个string压入StrVec的末尾
+  void pop_back();//从strVec的末尾弹出string
 
   void reserve(size_type);
   void resize(size_type, const std::string & = std::string());
@@ -25,10 +25,12 @@ public:
   bool empty() const { return cbegin() == cend(); }
   size_type size() const { return first_free - first_elem; }
   size_type capacity() const { return cap - first_elem; }
-
+  
+ // const std::string * 表明返回值是一个指向常量字符串的指针 不能通过*begin()改变其值 但是返回值可以指向其他内容 begin()++可以的
   std::string *begin() { return first_elem; }
   std::string *end() { return first_free; }
-  const std::string *begin() const { return first_elem; }
+  
+  const std::string *begin() const { return first_elem; }  
   const std::string *end() const { return first_free; }
   const std::string *cbegin() const { return begin(); }
   const std::string *cend() const { return end(); }
