396ef5e9be32bf10d0e9441c9e728cb0b685bd2f
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2021-01-08 15:41:08 +0800

    Update 15.14.cpp

--- ch15/15.14.cpp
+++ ch15/15.14.cpp
@@ -21,14 +21,19 @@ private:
 int main() {
   base bobj; base *bp1 = &bobj; base &br1 = bobj;
   derived dobj; base *bp2 = &dobj; base &br2 = dobj;
-  // (a) base::print();
+  
+  // (a) base::print(); 
   bobj.print(std::cout);
-  // (b) derived::print();
+  // (b) derived::print(); 不是指针或者引用
   dobj.print(std::cout);
+  
+  //name不是虚函数 都是从基类来的
   // (c) base::name();
   bp1->name();
   // (d) base::name();
   bp2->name();
+  
+  //br1 br2是指针或者引用 通过指针或引用调用虚函数print动态绑定
   // (e) base::print();
   br1.print(std::cout);
   // (f) derived::print();
