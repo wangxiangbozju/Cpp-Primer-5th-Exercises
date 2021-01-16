fe756ba2ea75cf34e53e637d7f3371219fe0ebe9
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2021-01-16 18:52:50 +0800

    Update AndQuery.cpp

--- ch15/15.39/AndQuery.cpp
+++ ch15/15.39/AndQuery.cpp
@@ -10,10 +10,9 @@
 #endif
 
 QueryResult AndQuery::eval(const TextQuery &t) const {
-#if DEBUG_LEVEL >= 1
-  std::cout << "AndQuery::eval" << std::endl;
-#endif
+  //虚调用（可能是递归调用） 
   auto left = lhs.eval(t), right = rhs.eval(t);
+  //ret_lines是指向set的一个指针
   auto ret_lines = std::make_shared<std::set<line_no_type>>();
   // NOTE we cannot use `back_inserter` here, because a `set` does not have a
   // `push_back` member function.
@@ -24,9 +23,7 @@ QueryResult AndQuery::eval(const TextQuery &t) const {
 }
 
 Query operator&(const Query &lhs, const Query &rhs) {
-#if DEBUG_LEVEL >= 1
-  std::cout << "Query operator&(const Query &, const Query &)" << std::endl;
-#endif
+
   // NOTE we cannot use `std::make_shared` here, because the type of the
   // dynamically created object and the type of the object pointed by shared
   // pointer are different.
