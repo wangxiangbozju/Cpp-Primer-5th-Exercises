10373fb67803cbc47705fcbe4f54a0c7cd07522b
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2021-01-16 15:37:00 +0800

    Update Query.h

--- ch15/15.39/Query.h
+++ ch15/15.39/Query.h
@@ -10,6 +10,8 @@ class Query_base;
 #include <iostream>
 
 //给用户查询的接口 提供接受string的构造函数（public) 和接受Query_base的指针的构造函数(private)
+//其内部的数据是静态类型为指向Query_base的指针 在运行时该指针绑定Query_base的不同派生类
+//其函数eval 和rep 是通过指向Query_base的指针调用Query_base的不同派生类override的虚函数
 class Query {
   //  operator~  operator& operator|都需要使用Query的构造函数 应该申明为友元函数
   friend Query operator~(const Query &);
