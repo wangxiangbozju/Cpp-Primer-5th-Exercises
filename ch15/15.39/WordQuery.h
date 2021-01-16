01b03e6f0c8b54acf0953e7263269ab04a643441
wangxiangbozju <43855661+wangxiangbozju@users.noreply.github.com>
2021-01-16 15:13:13 +0800

    Update WordQuery.h

--- ch15/15.39/WordQuery.h
+++ ch15/15.39/WordQuery.h
@@ -1,10 +1,6 @@
 #ifndef WORDQUERY_H
 #define WORDQUERY_H
 
-#if DEBUG_LEVEL >= 1
-#include <iostream>
-#endif
-
 #include <string>
 #include "TextQuery.h"
 #include "QueryResult.h"
@@ -12,26 +8,13 @@
 
 class WordQuery : public Query_base {
   friend class Query;
-
-  WordQuery(const std::string &s) : query_word(s) {
-#if DEBUG_LEVEL >= 1
-    std::cout << "WordQuery::WordQuery(const std::string &)" << std::endl;
-#endif
-  }
-
+  WordQuery(const std::string &s) : query_word(s) {}
   QueryResult eval(const TextQuery& t) const override {
-#if DEBUG_LEVEL >= 1
-    std::cout << "WordQuery::eval" << std::endl;
-#endif
     return t.query(query_word);
   }
   std::string rep() const override {
-#if DEBUG_LEVEL >= 1
-    std::cout << "WordQuery::rep" << std::endl;
-#endif
     return query_word;
   }
-
   std::string query_word;
 };
 
